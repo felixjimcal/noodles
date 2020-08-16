9aeb16e6981172e3e00135551bf4400ab863ac35
Felix JC <felixjcalvo@gmail.com>
2020-08-16 14:46:17 +0200

    Le indicamos a nuestro server donde estan nuestras views

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const path = require("path");
 // Initializations
 const app = express();
 
 // Settings 
 // Si existe un puerto, usalo, sino usa el 3000
 app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
 
 // Middlewares (Funciones que seran ejecutadas antes de llegar al servidor, Hooks al fin y al cabo
 
