778506756c5cd882483fbbc9f79af3505d183729
Felix JC <felixjcalvo@gmail.com>
2020-08-16 14:25:24 +0200

    new branch created
    index.js Structure created

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+// Initializations
+const app = express();
+
+// Settings 
+
+// Middlewares (Funciones que seran ejecutadas antes de llegar al servidor, Hooks al fin y al cabo
+
+// Global varaibles
+
+
+// Routes
+
+
+// Static Files
+
+
+// Server listenning
