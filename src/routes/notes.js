7d9f4fadc85b613dffcf073241e653dbd74fdfb1
Felix JC <felixjcalvo@gmail.com>
2020-08-19 12:31:56 +0200

    First POST example

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,5 +1,14 @@
 const router = require("express").Router(); // facilitar creacion rutas
 
+router.get("/notes/add", (req, res) => {
+    res.render("./notes/add.hbs");
+});
+
+router.post("/notes/add", (req, res) => {
+    console.log(req.body);
+    res.send("ok");
+});
+
 router.get("/notes", (req, res) => {
     res.send("notas");
 });
